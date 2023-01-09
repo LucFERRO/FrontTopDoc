22e971fe3dc6dd7c572e1f5227c02feb3812dd1f
LucFERRO <luchenri.ferro@gmail.com>
2023-01-09 15:49:32 +0100

    favicon + logo

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,17 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-        Template propre Next+Axios
-    </div>
-  )
+    return (
+        <>
+            <Head>
+                <title>TopDoc</title>
+                <link rel="icon" href="Pharmacy.svg" type="image/gif" />
+            </Head>
+
+            <div className={styles.container}>
+               TOPDOC
+            </div>
+            <Image src={'/Pharmacy.svg'} alt="logo" width="350px" height="300px"/>
+        </>
+    )
 }
